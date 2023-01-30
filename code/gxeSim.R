a753e1c7319d7188441be49ec83ffd64c827ee38
Moshood Agba Bakare <60209267+mab658@users.noreply.github.com>
2023-01-30 13:54:55 -0500

    Add files via upload

---
+++ code/gxeSim.R
@@ -0,0 +1,24 @@
+# function to simulate phenotypic data for GxE
+gxeSim <- function(pval1,pval2,pop,varE,nreps,nLocs){
+  stage <- deparse(substitute(pop))
+
+  GV = matrix(0, nrow=nInd(pop), ncol=nLocs)
+  dimnames(GV)<- list(pop@id,paste(paste0("L",1:nLocs)))
+
+  # A P-value of the environmental covariate
+  P <- round(seq(pval1, pval2, length.out=nLocs),1)
+
+  for (loc in 1:nLocs){  # begin loop
+    GV[,loc] = setPheno(pop=pop, varE=varE, reps=nreps, fixEff=year,
+                        p=P[loc], onlyPheno=TRUE, simParam=SP)
+  } #end loop
+
+  pop@pheno <- as.matrix(rowMeans(GV))
+  entryMeanVarE <- varE/nreps/nLocs
+  trialRec <- data.frame(id=pop@id,year=year,
+                         stage=stage,
+                         pheno=pop@pheno,errVar=entryMeanVarE)
+  rownames(trialRec) <- NULL
+  gxeOut <- list(pop,trialRec)
+  return(gxeOut)
+}
